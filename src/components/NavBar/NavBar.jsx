0c40e11dbfa87c6a60e08193dbee57121655edac
Lyndsay Ramberg <lyndsayramberg@Lyndsays-MacBook-Pro.local>
2019-07-16 10:57:01 -0700

    navbar css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './NavBar.module.css';
 
 const NavBar = (props) => {
     let nav = props.user ?
-        <div>
-            <Link to='/profile' className='NavBar-link'>Botanical Buddy</Link>
-            &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
-            <Link to='' className='NavBar-link' onClick={props.handleLogout}>LOG OUT</Link>
-            &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+        <div>           
+            <Link className={styles.logo} to='/profile' >BB</Link>
+            <Link to='' className={styles.logout} onClick={props.handleLogout}>LOG OUT</Link>
 
         </div>
         :
         <div>
             <Link to='/login' className='NavBar-link'>LOG IN</Link>
-            &nbsp;&nbsp;|&nbsp;&nbsp;
             <Link to='/signup' className='NavBar-link'>SIGN UP</Link>
         </div>;
 
 return (
-    <div className='NavBar'>
-    {nav}
+    <div className={styles.NavBar}>
+        {nav}
     </div>
 );
 }
